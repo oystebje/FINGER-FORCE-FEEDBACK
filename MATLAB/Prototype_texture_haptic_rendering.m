20c55be29733913e4faccb539416d1f6020e938c
oystebje <78421762+oystebje@users.noreply.github.com>
2021-04-08 17:04:37 +0200

    Add files via upload

---
+++ MATLAB/Prototype_texture_haptic_rendering.m
@@ -0,0 +1,25 @@
+%% PRototype code for testing algorithm in arduino sketch for rendering haptic texture
+
+x_min = -0.05; % [m]
+x_max = 0.05; % [m]
+
+texture_width = 0.005; %[m]
+
+xh = 0; % Position of handle x in m.
+xh_temp = 0;
+
+counter = 0;
+
+for xh = x_min:(texture_width/10):x_max
+    
+xh_temp = xt
+
+    if xh_temp == texture_width
+        counter = counter + texture_width;
+        
+        
+        
+    end
+    
+    
+    
